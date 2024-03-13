9f7a0e1fa702909a1a534fe78acce227e0b439df
AdityaMico <104138920+AdityaMico@users.noreply.github.com>
2024-03-13 19:23:01 +0700

    Add files via upload

---
+++ pengenalan_nama.java
@@ -0,0 +1,12 @@
+package mico;
+
+public class week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Halo, nama saya Mico Aditya Virmando");
+    	System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+    	System.out.println("Saya sedang menulis program Java");
+	}
+
+}
